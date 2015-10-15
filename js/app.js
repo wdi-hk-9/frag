df84df4319d66f5c318d448f3033e37de0f737b8
ianchoo <ianienchoo@gmail.com>
2015-10-15 11:21:35 +0800

    First complete working game. Bootstap styling and control menu added

--- js/app.js
+++ js/app.js
@@ -2,6 +2,16 @@
 */
 $(function () {
   'use strict';
+
+  // unselects the instructions button after modal is hidden
+  // 10ms delay before blur is triggered
+  $('#myModal').on('hidden.bs.modal', function() {
+    setTimeout(function () {
+      $('#instructions').trigger('blur');
+    }, 10);
+  })
+
+  // initialize game
   var game = createGame();
   game.init();
 });
